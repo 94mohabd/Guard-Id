6bc9ddca070f60457590e64f17c95210ca61da72
Mohammad Abdelrahman <albossmohammad83@gmail.com>
2025-02-17 09:05:42 -0500

    use env var

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -412,10 +412,10 @@ namespace GuardID.Controllers
 
             if (!System.IO.File.Exists(credentialPath))
             {
-                throw new FileNotFoundException($"Google Cloud Vision credentials file not found: {credentialPath}");
+                string base64Json = Environment.GetEnvironmentVariable("GOOGLE_CLOUD_VISION");
+                //throw new FileNotFoundException($"Google Cloud Vision credentials file not found: {credentialPath}");
             }
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
-
             // Convert processed image to base64 string
             OCRImage = ConvertJpgToBase64(idFilePath);
 
